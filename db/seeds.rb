5c373b01165a4a4154b685268cf98034d3f8e914
Felipe Ferreira <lfelipesferreira@gmail.com>
2022-08-30 14:48:20 +0200

    added seeds to our database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first
+felipe = User.new(full_name: "Felipe", email: "felipe@felipe.com", password: "123456", city: "Berlin", dog_breed: "Yorkshire")
+silvia = User.new(full_name: "silvia", email: "silvia@felipe.com", password: "123456", city: "Berlin", dog_breed: "Yorkshire")
+sara = User.new(full_name: "sara", email: "sara@felipe.com", password: "123456", city: "Berlin", dog_breed: "Yorkshire")
+hao = User.new(full_name: "Hao", email: "hao@felipe.com", password: "123456", city: "Berlin", dog_breed: "Yorkshire")
+
+felipe.save!
+silvia.save!
+sara.save!
+hao.save!
+
+dogwalker1 = DogWalker.new(price: 10, user:felipe)
+dogwalker2 = DogWalker.new(price: 5, user:silvia)
+dogwalker3 = DogWalker.new(price: 8, user:sara)
+dogwalker4 = DogWalker.new(price: 9, user:hao)
+
+dogwalker1.save!
+dogwalker2.save!
+dogwalker3.save!
+dogwalker4.save!
+
+
+booking1 = Booking.new(date:Date.today, confirmed: false, dog_walker: dogwalker1, user: sara)
+
+booking1.save!
