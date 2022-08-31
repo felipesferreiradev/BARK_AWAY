45a6ad23afadf530422794769ab5ae50ad37379e
Felipe Ferreira <lfelipesferreira@gmail.com>
2022-08-31 16:05:46 +0200

    Pitch ready

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,17 +16,18 @@ class BookingsController < ApplicationController
     @dog_walker = DogWalker.find(params[:dog_walker_id])
     @booking = Booking.new(booking_params)
     @booking.dog_walker = @dog_walker
+    @booking.user = current_user
     if @booking.save
-      redirect_to dog_walker_path(@dog_walk)
+      redirect_to dog_walker_path(@dog_walker), notice: "Your Dog Walk has been booked!"
+
     else
-      render "bookings/show", status: :unprocessable_entity
+      render "bookings/new", status: :unprocessable_entity
     end
   end
 
   private
 
-  def review_params
-    params.require(:booking).permit(:dog_walker_id, :date, :confirmed)
+  def booking_params
+    params.require(:booking).permit(:dog_walker_id, :date, :confirmed, :notes, :dog_breed, :user_id)
   end
-
 end
