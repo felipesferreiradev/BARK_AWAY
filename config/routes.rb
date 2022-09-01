c7b7952fccd2f09159f1adeeed8c6a24b361af2c
Hao Wang <wanghao960225@gmail.com>
2022-09-01 18:34:08 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-<<<<<<< HEAD
-<<<<<<< HEAD
-  resources :dog_walkers, only: [:index, :show, :new, :create] do
-=======
-=======
->>>>>>> origin
   get "/dashboard", to: "pages#dashboard"
-  resources :dog_walkers, only: [:index, :show] do
->>>>>>> b64b2079ea6704347eb33dd76c30d22c2b30d344
+  resources :dog_walkers, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:index, :new, :create, :show, :destroy]
   end
 end
