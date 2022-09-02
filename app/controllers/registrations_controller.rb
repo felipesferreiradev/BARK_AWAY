d19f72aafec5a2e6fb68b08612916d6cb30f8ce2
Hao Wang <wanghao960225@gmail.com>
2022-09-02 14:40:34 +0200

    user can edit their information now

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
 
   protected
-  def after_update_path_for(resource)
-      flash[:notice] = "Account succesfully updated"
-      new_dog_walker_path
+
+  def after_update_path_for(_resource)
+    flash[:notice] = "Account succesfully updated"
+    new_dog_walker_path
   end
 end
